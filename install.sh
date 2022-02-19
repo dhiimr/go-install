014a4893d7acca75e5ae9e67ccc2ba2593bded16
Edi Susanto <51095316+dhiimr@users.noreply.github.com>
2022-02-19 17:10:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-
 wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go get github.com/prasmussen/gdri
+export PATH=$PATH:/home/mrzakadhiimr/gopath/bin
